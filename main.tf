8bc02d3d660b83b58ca1e69415e9b74219b71cef
pehks1980 <pehks1980@gmail.com>
2023-10-31 00:32:34 +1100

    elb plus asg

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ resource "aws_launch_configuration" "example" {
 	image_id = "ami-40d28157"
 	instance_type = "t2.micro"
 
-	security_groups = ["${aws_security_group.instance.id}","${aws_security_group.ssh.id}"]
+	security_groups = ["${aws_security_group.instance.id}","${aws_security_group.ssh.id}","${aws_security_group.elb.id}"]
 
 	key_name  = "myec2key"
 	user_data = <<-EOF
@@ -67,7 +67,10 @@ resource "aws_autoscaling_group" "example" {
 	launch_configuration = "${aws_launch_configuration.example.id}"
         #list all zones
 	availability_zones = ["${data.aws_availability_zones.all.names[0]}","${data.aws_availability_zones.all.names[1]}"]
-	
+	#elb
+	load_balancers = ["${aws_elb.example.name}"]
+	health_check_type = "ELB"
+
 	#instances range min max for autoscaling
 	min_size = 2
 	max_size = 3
@@ -79,7 +82,56 @@ resource "aws_autoscaling_group" "example" {
 	}
 } 
 
-#output "public_ip" {
-#	value = "${aws_instance.example.public_ip}"
-#}
+#ELB
+resource "aws_elb" "example" {
+	name = "tf-elb-example"
+        availability_zones = ["${data.aws_availability_zones.all.names[0]}","${data.aws_availability_zones.all.names[1]}"]
+	
+	listener { #redir 80->8081 (http)
+		lb_port       = 80
+		lb_protocol   = "http"
+		instance_port = "${var.s_port}"
+		instance_protocol = "http"
+	}
+	
+	health_check {
+		healthy_threshold = 2
+		unhealthy_threshold = 2
+		timeout = 3
+		interval = 30
+		target = "HTTP:${var.s_port}/"
+	}
+
+}
+
+#ELB sec_group to allow 80 port
+
+resource "aws_security_group" "elb" {
+  name        = "ELBSecurityGroup"
+  description = "Allow 80 for ELB access"
+  
+  	ingress {
+    		from_port   = 80
+    		to_port     = 80
+    		protocol    = "tcp"
+    		cidr_blocks = ["0.0.0.0/0"] # You can restrict this to your specific IP range for better security
+ 	}
+	
+	egress {
+		from_port   = 0
+    		to_port     = 0
+    		protocol    = "-1"
+    		cidr_blocks = ["0.0.0.0/0"] 
+	}
+
+	#lifecycle {
+	#	create_before_destroy = true
+	#}
+}
+
+
+
+output "public_dns" {
+	value = "${aws_elb.example.dns_name}"
+}
 
