2a146baeb0e92dc9ecad439e2256d1677e5d6d8d
pehks1980 <pehks1980@gmail.com>
2023-10-30 18:22:47 +1100

    variables

--- main.tf
+++ main.tf
@@ -2,6 +2,11 @@ provider "aws" {
   region = "us-east-1"
 }
 
+variable "s_port" {
+	description = "server listen port (http)"
+	default = "8081"
+}
+
 resource "aws_instance" "example" {
 	ami = "ami-40d28157"
 	instance_type = "t2.micro"
@@ -17,15 +22,15 @@ resource "aws_instance" "example" {
 	user_data = <<-EOF
 		#!/bin/bash
 		echo "Hello World" > index.html
-		nohup busybox httpd -f -p 8080 &
+		nohup busybox httpd -f -p "${var.s_port}" &
 		EOF
 }	
 
 resource "aws_security_group" "instance" {
 	name = "tf1-example-instance"
 	ingress {
-		from_port = 8080
-		to_port = 8080
+		from_port = "${var.s_port}"
+		to_port = "${var.s_port}"
 		protocol = "tcp"
 		cidr_blocks = ["0.0.0.0/0"] #any IP allowed
 	}
@@ -42,4 +47,8 @@ resource "aws_security_group" "ssh" {
     cidr_blocks = ["0.0.0.0/0"] # You can restrict this to your specific IP range for better security
  }
 }
+
+#output "public_ip" {
+#	value = "${aws_instance.example.public_ip}"
+#}
 
