419eb5e68ff46b87d15729e24799a5663f08129b
pehks1980 <pehks1980@gmail.com>
2023-10-30 19:56:12 +1100

    autoscale group 2-3 config

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
+
+data "aws_availability_zones" "all" {
+
+}
+
 provider "aws" {
   region = "us-east-1"
 }
@@ -7,23 +12,22 @@ variable "s_port" {
 	default = "8081"
 }
 
-resource "aws_instance" "example" {
-	ami = "ami-40d28157"
+resource "aws_launch_configuration" "example" {
+	image_id = "ami-40d28157"
 	instance_type = "t2.micro"
 
-	vpc_security_group_ids = ["${aws_security_group.instance.id}","${aws_security_group.ssh.id}"]
+	security_groups = ["${aws_security_group.instance.id}","${aws_security_group.ssh.id}"]
 
-	tags = {
-    		Name        = "MyEC2Instance"
-    		Environment = "Production"
-    		Owner       = "John Doe"
-  	}
 	key_name  = "myec2key"
 	user_data = <<-EOF
 		#!/bin/bash
 		echo "Hello World" > index.html
 		nohup busybox httpd -f -p "${var.s_port}" &
 		EOF
+
+	lifecycle {
+		create_before_destroy = true
+	}
 }	
 
 resource "aws_security_group" "instance" {
@@ -34,19 +38,46 @@ resource "aws_security_group" "instance" {
 		protocol = "tcp"
 		cidr_blocks = ["0.0.0.0/0"] #any IP allowed
 	}
+
+	lifecycle {
+		create_before_destroy = true
+	}
+	
 }
 
 resource "aws_security_group" "ssh" {
   name        = "SSHSecurityGroup"
   description = "Allow SSH access"
   
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # You can restrict this to your specific IP range for better security
- }
+  	ingress {
+    		from_port   = 22
+    		to_port     = 22
+    		protocol    = "tcp"
+    		cidr_blocks = ["0.0.0.0/0"] # You can restrict this to your specific IP range for better security
+ 	}
+
+	lifecycle {
+		create_before_destroy = true
+	}
 }
+
+#create (A)uto(S)caling(G)roup
+resource "aws_autoscaling_group" "example" {
+	#get linked launch config with resource ubuntu
+	launch_configuration = "${aws_launch_configuration.example.id}"
+        #list all zones
+	availability_zones = ["${data.aws_availability_zones.all.names[0]}","${data.aws_availability_zones.all.names[1]}"]
+	
+	#instances range min max for autoscaling
+	min_size = 2
+	max_size = 3
+
+	tag {
+		key = "ASG"
+		value = "tf-asg-example"
+		propagate_at_launch = true
+	}
+} 
 
 #output "public_ip" {
 #	value = "${aws_instance.example.public_ip}"
