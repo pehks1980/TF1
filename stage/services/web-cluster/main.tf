b931cd863276f79721bcd9c1dcc574c5ebe6c6e9
pehks1980 <pehks1980@gmail.com>
2023-11-06 16:15:20 +1100

    ref as commit id

--- stage/services/web-cluster/main.tf
+++ stage/services/web-cluster/main.tf
@@ -4,7 +4,8 @@ provider "aws" {
 #branch test
 module "web_cluster" {
 	#source = "../../../modules/services/web-cluster"
-	source = "git::https://github.com/pehks1980/TF1-web_cluster.git//web-cluster?ref=v0.0.1&branch=test"
+	#source = "git::https://github.com/pehks1980/TF1-web_cluster.git//web-cluster?ref=test"
+	source = "git::https://github.com/pehks1980/TF1-web_cluster.git//web-cluster?ref=13cc8305c7cf8c1c845047699efda95908ae1ad0"
 	cluster_name = var.cluster_name
 	db_remote_state_bucket = var.db_remote_state_bucket
 	db_remote_state_bucket_key = var.db_remote_state_bucket_key
