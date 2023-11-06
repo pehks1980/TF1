13cc8305c7cf8c1c845047699efda95908ae1ad0
pehks1980 <pehks1980@gmail.com>
2023-11-06 15:59:12 +1100

    branch test

--- stage/services/web-cluster/main.tf
+++ stage/services/web-cluster/main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
   region = "us-east-1"
 }
-
+#branch test
 module "web_cluster" {
 	#source = "../../../modules/services/web-cluster"
-	source = "git::https://github.com/pehks1980/TF1-web_cluster.git//web-cluster?ref=v0.0.1"
+	source = "git::https://github.com/pehks1980/TF1-web_cluster.git//web-cluster?ref=v0.0.1&branch=test"
 	cluster_name = var.cluster_name
 	db_remote_state_bucket = var.db_remote_state_bucket
 	db_remote_state_bucket_key = var.db_remote_state_bucket_key
